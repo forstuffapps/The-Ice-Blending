0657cef1c8301d7e07301ee33ad6172a1fd18438
forstuffapps <forstuffapps@gmail.com>
2023-11-22 01:40:14 -0500

    changed some files

--- javascript_files/show_json.js
+++ javascript_files/show_json.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
    //$.getJSON("javascript_files/team.json", function(data){
-    $.getJSON("https://cc1-s3-bucket-1.s3.us-east-2.amazonaws.com/team.json", function(data){
+    $.getJSON("./javascript_files/team.json", function(data){
      $.each(data.teammembers, function(key, value) {
        $("#team").append(
          '<div class="team-member">' +
